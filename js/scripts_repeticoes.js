86df9f5ed1546e12490a95a6147b24be6eeb073b
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-15 19:44:52 -0300

    adicionado estruturas de repetição, contador e acumulador e repetição com while no arquivo scripts_repeticoes.js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,1 +1,50 @@
+//Pegando elemenos do DOM
+const divFor = document.querySelector('#div-for')
+
 //Estruturas de Repetições
+//for(i = 10; i < 10; i++){
+    //console.log(i)
+    //divFor.innerHTML += `${i} <br>`
+//}
+
+//Contador e Acumulador
+const inputNum1 = document.querySelector('#num1')
+const btnNum1 = document.querySelector('#btn-num1')
+const divResult = document.querySelector('#result-cont-acum')
+
+let cont = 0, contPar = 0, contImpar = 0
+let acum = 0.0
+
+btnNum1.addEventListener('click', (evt)=>{
+    let numDigitado = Number(inputNum1.value)
+
+    //Declração de Contador
+    cont++
+    //Declaração de Acumulador
+    acum += numDigitado
+    //acum -= numDigitado para subtrair
+
+    //numDigitado % 2 == 0 significa se o num Digitado dividido por dois sobra 0
+    if(numDigitado % 2 == 0){
+        contPar++
+    }else{
+        contImpar++
+    }
+
+    console.log(numDigitado)
+
+    divResult.innerHTML = `Total de números digitados ${cont} <br> A soma dos números digitados ${acum} <br> Par digitado: ${contPar} <br> Ímpar digitado: ${contImpar} <br>`
+    divFor.innerHTML += `${numDigitado} <br>`
+})
+
+//Repetição com While
+const inputFrase = document.querySelector('#msg')
+const inputNumRepeticao = document.querySelector('#numRepeticao')
+const btnExibir = document.querySelector(`#btn-exibir`)
+const divResultFrase = document.querySelector(`#result-frase`)
+
+btnExibir.addEventListener('click', (evt)=>{
+    
+
+    divResultFrase.innerHTML
+})
